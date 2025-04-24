1a63211a4d77e1a74354d7cc04b626e92c3c1bd7
jumashafara <jumashafara0@gmail.com>
2025-04-24 21:35:49 +0300

    fix: updated static files dir

--- main/urls.py
+++ main/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from pip._vendor.urllib3.util import Url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('datasets.urls')),
-]
+] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
